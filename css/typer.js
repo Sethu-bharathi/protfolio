874456758dee3254b8a508893545630a1e776888
Sethu-bharathi <bharathisethu018@gmail.com>
2022-01-17 12:26:14 +0530

    first sommit

---
+++ css/typer.js
@@ -0,0 +1,46 @@
+const words = [" Developer", " Designer", " Programmer"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function Typer() {
+  if (count === words.length) {
+    count = 0;
+  }
+  currentText = words[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".typer").textContent = letter;
+
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+    setTimeout(Typer, 2000);
+  } else {
+    setTimeout(Typer, 300);
+  }
+})();
+
+function showall() {
+  setTimeout(() => {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("loaded").style.display = "block";
+  }, 2000);
+}
+let menutoggler=document.querySelector(".toggle");
+let mobilenav=document.querySelector(".mobilenavcontainer");
+
+menutoggler.onclick=function(){
+  menutoggler.classList.toggle("active");
+  mobilenav.classList.toggle("active");
+}
+
+let anav=document.querySelectorAll(".anav");
+
+for (let i = 0; i < anav.length; i++) {
+  anav[i].addEventListener("click",()=>{
+    menutoggler.classList.toggle("active");
+    mobilenav.classList.toggle("active");
+  })
+}
